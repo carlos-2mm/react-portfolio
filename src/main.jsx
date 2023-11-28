2b1ff1111b8e5a8a2b484fb328533aa9c4d8976a
Carlos Macias <cimm21187@gmail.com>
2023-11-28 16:53:13 -0500

    feat:set up  the main App.jsx, App.css, main.jsx, index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 // Importing the font CSS
 import "@fontsource/poppins/400.css";
@@ -10,6 +11,8 @@ import "@fontsource/poppins/800.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
