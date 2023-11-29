e664f14ab8e6fc1050ed55f6a1eabb3d3c199661
Carlos Macias <cimm21187@gmail.com>
2023-11-29 14:09:00 -0500

    feat(About): add About component

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <div className="pic-container">
+      <img className="about-pic" src="./public/img/Carlos-Macias.jpg" alt="Carlos Macias" />
+      </div>
+      <p>Hello, I'm</p>
+      <h3>Carlos Macias</h3>
+      <p>Full-Stack Developer</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        voluptates, quos, quae, quibusdam quia voluptas doloribus natus
+        voluptatem quod voluptatibus quidem? Quam, quibusdam. Quisquam
+        voluptatum, voluptate quia quibusdam quae quas?
+      </p>
+    </section>
+  );
+}
+
+export default About;
