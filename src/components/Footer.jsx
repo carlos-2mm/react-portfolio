0e74e6db3a87b4f6a86628f1cba6697dcebb12e8
Carlos Macias <cimm21187@gmail.com>
2023-11-29 10:33:52 -0500

    feat(Footer): add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/carlos-2mm">GitHub</a>
+      <a href="https://linkedin.com/in/carlos-macias-m">LinkedIn</a>
+      <a href="https://twitter.com/carlos_imm">Twitter</a>
+    </footer>
+  );
+}
+
+export default Footer;
