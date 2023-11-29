b55110d8d42fa278def21c6d2a66a6c21f2d8e9a
Carlos Macias <cimm21187@gmail.com>
2023-11-29 11:46:09 -0500

    feat(Footer): add dependency to import icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {BsGithub,BsLinkedin,BsTwitterX} from "react-icons/bs";
 
 function Footer() {
   return (
     <footer>
-      <a href="https://github.com/carlos-2mm">GitHub</a>
-      <a href="https://linkedin.com/in/carlos-macias-m">LinkedIn</a>
-      <a href="https://twitter.com/carlos_imm">Twitter</a>
+      <a href="https://github.com/carlos-2mm"><BsGithub /></a>
+      <a href="https://linkedin.com/in/carlos-macias-m"><BsLinkedin /></a>
+      <a href="https://twitter.com/carlos_imm"><BsTwitterX /></a>
     </footer>
   );
 }
