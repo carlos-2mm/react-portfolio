f8b1b7a29a685068e694d91b93bfa5a7e07b0964
Carlos Macias <cimm21187@gmail.com>
2024-01-16 12:33:09 -0500

    change router from React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
 import React, {useState} from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import About from './pages/About'
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
 import Footer from './components/Footer';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('About');
-
-  const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
-    }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />;
-    }
-    return <Resume />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
     <div>
-      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-      <section className='content'>
-      <main>{renderPage()}</main>
-      </section>
+      <Header />
+      <div className="main-container">
+      <Outlet />
+      </div>
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Contact from "./pages/Contact.jsx";
 
 // Importing the font CSS
 import "@fontsource/poppins/400.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
-);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/portfolio",
+          element: <Portfolio />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+
+  const Main = () => {
+    return (
+      <RouterProvider router={router} />
+    );
+  }
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
