eaac590b4b8930daed22ff6be1ba604aed534016
Carlos Macias <cimm21187@gmail.com>
2024-01-23 01:39:31 -0500

    add projects.js an array of projects por portfolio

---
+++ src/utils/projects.js
@@ -0,0 +1,120 @@
+export const frontEndProjects = [
+  {
+    id: 1,
+    title: "Weather Dashboard",
+    image: "/Weather-Dashboard.png",
+    link: "https://carlos-2mm.github.io/weather-forecast-app/",
+    repo: "https://github.com/carlos-2mm/weather-forecast-app",
+  },
+  {
+    id: 2,
+    title: "Daily Agenda Assistant",
+    image: "/Daily-Agenda-Assistant.png",
+    link: "https://carlos-2mm.github.io/daily-agenda-assistant/",
+    repo: "https://github.com/carlos-2mm/daily-agenda-assistant",
+  },
+  {
+    id: 3,
+    title: "JavaScript Quiz",
+    image: "/JavaScript-Quiz.png",
+    link: "https://carlos-2mm.github.io/js-fundamentals-quiz/",
+    repo: "https://github.com/carlos-2mm/js-fundamentals-quiz",
+  },
+  {
+    id: 4,
+    title: "Password Generator",
+    image: "/Password-Generator.png",
+    link: "https://carlos-2mm.github.io/safe-password-creator/",
+    repo: "https://github.com/carlos-2mm/safe-password-creator",
+  },
+  {
+    id: 5,
+    title: "Personal Portfolio",
+    image: "/Portfolio.png",
+    link: "project_link",
+    repo: "github_repo_link",
+  },
+  {
+    id: 6,
+    title: "Horiseon",
+    image: "/Horiseon.png",
+    link: "https://carlos-2mm.github.io/horiseon-online-specialist/t_link",
+    repo: "https://github.com/carlos-2mm/horiseon-online-specialist",
+  },
+];
+
+export const backEndProjects = [
+  {
+    id: 1,
+    title: "Text Editor",
+    image: "/J.A.T.E..png",
+    link: "https://jate-txt-edit-4d9b48ee6fe7.herokuapp.com/",
+    repo: "https://github.com/carlos-2mm/text-editor",
+  },
+  {
+    id: 2,
+    title: "Social Network API",
+    image: "/Social-Network-API.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/social-network-API",
+  },
+  {
+    id: 3,
+    title: "E-Commerce Backend",
+    image: "/e-commerce.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/e-commerce-backend",
+  },
+  {
+    id: 4,
+    title: "Employee Tracker",
+    image: "/Employee-Tracker.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/employee-tracker",
+  },
+  {
+    id: 5,
+    title: "Note Taker",
+    image: "/landing_page.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/note-taker",
+  },
+  {
+    id: 6,
+    title: "SVG Logo Maker",
+    image: "/SVG-Logo-Maker.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/svg-logo-maker",
+  },
+  {
+    id: 7,
+    title: "README Generator",
+    image: "/README-Generator.png",
+    link: "project_link",
+    repo: "https://github.com/carlos-2mm/readme-generator",
+  },
+];
+
+export const fullStackProjects = [
+  {
+    id: 1,
+    title: "The CodeX",
+    image: "/The-Codex.png",
+    link: "https://the-codex.onrender.com/",
+    repo: "https://github.com/theApostols/the-codex",
+  },
+  {
+    id: 2,
+    title: "TravIs",
+    image: "/travis_app.png",
+    link: "https://travis231017-9762a24c277b.herokuapp.com/",
+    repo: "https://github.com/UofTTeam2/TravIS",
+  },
+  {
+    id: 3,
+    title: "Cine Search",
+    image: "/CineSearch.png",
+    link: "https://team-ccmy.github.io/Cine-Search/",
+    repo: "https://github.com/Team-ccmy/Cine-Search",
+  },
+];
