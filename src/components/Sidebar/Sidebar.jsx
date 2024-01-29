9f712982fa1b7c9e756755d4eaeec7ffd7cf73ac
Carlos Macias <cimm21187@gmail.com>
2024-01-29 14:32:26 -0500

    update some routes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Links from "./Sidebar/Links";
+import Links from "./Links";
 import "./Sidebar.css";
 import ToggleButton from "./ToggleButton";
 
